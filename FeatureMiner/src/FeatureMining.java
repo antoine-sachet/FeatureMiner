52e81c6e3369ebc5c895ce222248ea6938e20d14
Antoine Sachet <antoine.sac@gmail.com>
2015-04-10 19:44:30 +0100

    feature is pruned if in the 1000 most common nouns

--- FeatureMiner/src/FeatureMining.java
+++ FeatureMiner/src/FeatureMining.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -15,42 +19,55 @@ import Tagger.Util;
 public class FeatureMining {
 
 	public static void main(String[] args) throws IOException {
-		
-		
-		
+
+
+
 		// directory containing our review database (json files) 
-		String dataPath = "D:/Sachou/UCL/feature-extraction-from-product-review_data-mining-project/data/tablets";
-		
+		String dataPath = "D:\\Sachou\\UCL\\FeatureMiner\\data\\small_laptop_data";
+
 		// path to product reviews (several reviews relative to the same product, in a json file)
-		String reviewPath = "D:/Sachou/UCL/feature-extraction-from-product-review_data-mining-project/data/tablets/B00AA6OVAO.json";
-		
+		String reviewPath = "D:\\Sachou\\UCL\\FeatureMiner\\data\\full_laptop_data\\B004O3BIBY.json";
+
 		// extracting the possible features from our database
 		FeatureExtractor extractor = new FeatureExtractor();
 		ArrayList<String> features = extractor.extractFeatures(dataPath);
+
+		ArrayList<String> commonNouns = readFile("frequent_nouns", 1000);
+		ArrayList<String> cleanFeatures = new ArrayList<String>();
+		
 		
 		for(String feat: features){
-			
-			System.out.println(feat);
+			Boolean toRemove = false;
+			for(String noun: commonNouns){
+				//System.out.println(noun);
+				if(feat.equals(noun)){
+					toRemove=true;
+					System.out.println("Feature \""+feat+"\" was filtered out as too common.");
+				}
+			}
+			if(!toRemove){
+				cleanFeatures.add(feat);
+			}
 		}
-		
+		System.out.println((features.size()-cleanFeatures.size())+"/"+features.size()+" features were removed.");
 		// loading up the reviews, each one in a string
 		ArrayList<String> reviews = getRawReviews(reviewPath);
-		
+
 		// finding the opinion for each review
 		// The opinion of a review is an arraylist of (sentence, sentiment)
 		SentimentAnalyser analyser = new SentimentAnalyser();
 		ArrayList<Opinion> opinions = analyser.findOpinion(reviews);
-		
+
 		// Finding the features rating given the opinions
-		FeatureRater rater = new FeatureRater(features);
+		FeatureRater rater = new FeatureRater(cleanFeatures);
 		Summary summary = rater.summarize(opinions);
-		
+
 
 		// Printing out the summary
 		System.out.println(summary);
-		
+
 	}
-	
+
 	public static ArrayList<String> getRawReviews(String filePath) {
 		String str = new Util().ReadFile(filePath);
 		JSONObject raw = JSONObject.fromObject(str);
@@ -63,6 +80,24 @@ public class FeatureMining {
 			reviews.add(rev);
 		}
 		return(reviews);
+	}
+
+	public static ArrayList<String> readFile(String filename, int nLines) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new FileReader(filename));
+		ArrayList<String> lines = new ArrayList<String>();
+		try {
+			String line = br.readLine();
+			int i=0;
+			while (line != null && i<nLines) {
+				lines.add(line.trim());
+				i++;
+				line = br.readLine();
+			}
+		} finally {
+			br.close();
+		}
+		return(lines);
 	}
 
 }
